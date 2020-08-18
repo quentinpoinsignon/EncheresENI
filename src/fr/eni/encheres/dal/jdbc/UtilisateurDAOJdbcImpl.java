d48cddb5e7039d3e2d08ba95bf9080ac869a5339
Jérôme Arrigoni <61696249+JeromeArrigoni@users.noreply.github.com>
2020-08-18 14:24:48 +0200

    Création fonction insert utilisateur
    Création de la requête permettant d'enregistrer un nouvel utilisateur dans la base de données.

--- src/fr/eni/encheres/dal/jdbc/UtilisateurDAOJdbcImpl.java
+++ src/fr/eni/encheres/dal/jdbc/UtilisateurDAOJdbcImpl.java
@@ -1,10 +1,10 @@
 package fr.eni.encheres.dal.jdbc;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 
+import fr.eni.encheres.bo.Utilisateur;
 import fr.eni.encheres.dal.interfaces.UtilisateurDAO;
 
 /**
@@ -14,51 +14,88 @@ import fr.eni.encheres.dal.interfaces.UtilisateurDAO;
 
 public class UtilisateurDAOJdbcImpl implements UtilisateurDAO {
 
+	public static Connection getConnection() throws SQLException {
+
+		return JdbcTools.getConnection();
+
+	}
+
 	@Override
-	public void userConnection(String email, String password) throws SQLException {
-		String userEmail = email; // Il faut récupérer les informations
-		String userPassword = password; // rentrées dans le formulaire par l'utilisateur
+	public void userInsert(Utilisateur user) throws Exception {
 
-		ResultSet requestResult = null;
+		final String USER_INSERT = "INSERT INTO UTILISATEURS(pseudo,nom,prenom,email,telephone,rue,code_postal,ville,mot_de_passe,credit,administrateur) VALUES(?,?,?,?,?,?,?,?)";
 
-		String verifEmailRequest = " SELECT email FROM UTILISATEURS WHERE email =" + userEmail;
-		String VerifPassword = "SELECT mot_de_passe FROM UTILISATEURS WHERE email =" + userPassword;
-		String RecupUserInfo = "SELECT pseudo,nom,prenom FROM UTILISATEURS WHERE email = " + userEmail
-				+ " AND mot_de_passe =" + userPassword;
+		try (Connection databaseConnection = getConnection();
+				PreparedStatement preparedStatement = databaseConnection.prepareStatement(USER_INSERT);) {
 
-		Connection connect = JdbcTools.getConnection();
+			// Valoriser les paramÃ¨tres de la requete
 
-		Statement stmt = connect.createStatement();
-		requestResult = stmt.executeQuery(verifEmailRequest);
+			preparedStatement.setString(1, user.getPseudo());
+			preparedStatement.setString(2, user.getNom());
+			preparedStatement.setString(3, user.getPrenom());
+			preparedStatement.setString(4, user.getEmail());
+			preparedStatement.setString(5, user.getTelephone());
+			preparedStatement.setString(6, user.getRue());
+			preparedStatement.setString(7, user.getCodePostal());
+			preparedStatement.setString(8, user.getVille());
+			preparedStatement.setString(9, user.getMotDePasse());
+			preparedStatement.setInt(10, user.getCredit());
+			preparedStatement.setBoolean(5, user.getAdministrateur());
 
-		if (requestResult != null) {
+			preparedStatement.executeUpdate();
 
-			System.out.println("Email existe");
-			requestResult = stmt.executeQuery(VerifPassword);
+		} catch (SQLException e) {
 
-			if (requestResult != null) {
-				System.out.println("Mot de passe ok");
-				requestResult = null;
-				requestResult = stmt.executeQuery(RecupUserInfo);
+			throw new Exception("Erreur lors de l'ajout de l'utilisateur");
 
-				Boolean nextResult = requestResult.next();
-
-				while (nextResult) {
-
-					// Création de la session
-
-					requestResult.getString(1);
-					requestResult.getString(2);
-					requestResult.getString(3);
-
-					nextResult = requestResult.next();
-
-				}
-
-				requestResult.close();
-
-			}
 		}
 
 	}
+
+	/**
+	 * 
+	 * String userEmail = email; // Il faut récupérer les informations String
+	 * userPassword = password; // rentrées dans le formulaire par l'utilisateur
+	 * 
+	 * ResultSet requestResult = null;
+	 * 
+	 * String verifEmailRequest = " SELECT email FROM UTILISATEURS WHERE email =" +
+	 * userEmail; String VerifPassword = "SELECT mot_de_passe FROM UTILISATEURS
+	 * WHERE email =" + userPassword; String RecupUserInfo = "SELECT
+	 * pseudo,nom,prenom FROM UTILISATEURS WHERE email = " + userEmail + " AND
+	 * mot_de_passe =" + userPassword;
+	 * 
+	 * Connection connect = JdbcTools.getConnection();
+	 * 
+	 * Statement stmt = connect.createStatement(); requestResult =
+	 * stmt.executeQuery(verifEmailRequest);
+	 * 
+	 * if (requestResult != null) {
+	 * 
+	 * System.out.println("Email existe"); requestResult =
+	 * stmt.executeQuery(VerifPassword);
+	 * 
+	 * if (requestResult != null) { System.out.println("Mot de passe ok");
+	 * requestResult = null; requestResult = stmt.executeQuery(RecupUserInfo);
+	 * 
+	 * Boolean nextResult = requestResult.next();
+	 * 
+	 * while (nextResult) {
+	 * 
+	 * // Création de la session
+	 * 
+	 * requestResult.getString(1); requestResult.getString(2);
+	 * requestResult.getString(3);
+	 * 
+	 * nextResult = requestResult.next();
+	 * 
+	 * }
+	 * 
+	 * requestResult.close();
+	 * 
+	 * } }
+	 * 
+	 * }
+	 * 
+	 */
 }
