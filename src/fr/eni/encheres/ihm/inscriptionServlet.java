3270f31e11ac92a0f95ad60785d64902d1f2fba2
regisbonin <regis.bonin2020@campus-eni.fr>
2020-08-18 14:40:37 +0200

    regis

--- src/fr/eni/encheres/ihm/inscriptionServlet.java
+++ src/fr/eni/encheres/ihm/inscriptionServlet.java
@@ -1,11 +1,15 @@
 package fr.eni.encheres.ihm;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import fr.eni.encheres.bo.Utilisateur;
 
 /**
  * Servlet implementation class inscriptionServlet
@@ -35,9 +39,16 @@ public class inscriptionServlet extends HttpServlet {
         String rue = request.getParameter("rue");
         String ville = request.getParameter("ville");
         String confirmation = request.getParameter("confirmation");
-
-
-
+        String message;
+        
+        if (password != confirmation) {
+        	
+        	message = "La confirmation est différente du mot de passe!";
+			this.getServletContext().getRequestDispatcher("/WEB-INF/pages/login.jsp").forward(request, response);
+        }
+        Utilisateur utilisateur = new Utilisateur(pseudo, nom, prenom, email, telephone, rue, codepostal, ville, password);
+        request.getRequestDispatcher("/WEB-INF/pages/listeEnchres.jsp").forward(request, response);
+        System.out.println(utilisateur.getNom());
 	}
 
 }
