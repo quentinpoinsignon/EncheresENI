ff2ae396dd65a6462b5d7cc61f0f67d9ca405fa1
regisbonin <regis.bonin2020@campus-eni.fr>
2020-08-19 17:28:01 +0200

    profilModif

--- src/fr/eni/encheres/ihm/profilModifServlet.java
+++ src/fr/eni/encheres/ihm/profilModifServlet.java
@@ -7,27 +7,48 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import fr.eni.encheres.bll.UtilisateurManager;
+import fr.eni.encheres.bo.Utilisateur;
+
 /**
  * Servlet implementation class profilModifServlet
  */
-@WebServlet("/profilModifServlet")
+@WebServlet("/profilModif")
 public class profilModifServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	private UtilisateurManager uMger = new UtilisateurManager();
     
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String pseudo = request.getParameter("pseudo");
+	    String nom = request.getParameter("nom");
+	    String prenom = request.getParameter("prenom");
+	    String email = request.getParameter("email");
+		String telephone = request.getParameter("telephone");
+		String rue = request.getParameter("rue");
+        String codepostal = request.getParameter("codepostal");
+        String ville = request.getParameter("ville");
+		
+		request.setAttribute("pseudo", pseudo);
+		request.setAttribute("nom", nom);
+		request.setAttribute("prenom", prenom);
+		request.setAttribute("email", email);
+		request.setAttribute("telephone", telephone);
+		request.setAttribute("rue", rue);
+		request.setAttribute("codepostal", codepostal);
+		request.setAttribute("ville", ville);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+    	Utilisateur utilisateur;
+		//uMger.upDateUtilisateur(utilisateur);
+
 	}
 
 }
