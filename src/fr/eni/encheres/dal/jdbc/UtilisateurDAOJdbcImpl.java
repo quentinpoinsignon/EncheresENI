f6372f223b719712d3e9c8ab4f6289ec929ca09d
Jérôme Arrigoni <61696249+JeromeArrigoni@users.noreply.github.com>
2020-08-19 09:52:58 +0200

    Méthode userConnectionPseudo
    Implémentation de la méthode permettant de vérifier si un utilisateur est enregistré dans la base de données grâce à son pseudo et son mot de passe

--- src/fr/eni/encheres/dal/jdbc/UtilisateurDAOJdbcImpl.java
+++ src/fr/eni/encheres/dal/jdbc/UtilisateurDAOJdbcImpl.java
@@ -9,14 +9,32 @@ import fr.eni.encheres.bo.Utilisateur;
 import fr.eni.encheres.dal.interfaces.UtilisateurDAO;
 
 /**
- * Classe implémentant l'interface UtilisateurDAO et contenant les différentes
- * méthodes permettant de gérer les requêtes SQL liées aux utilisateurs
+ * @author jarrigon2020
+ * 
+ *
+ *         Classe implémentant l'interface UtilisateurDAO et contenant les
+ *         différentes méthodes permettant de gérer les requêtes SQL liées aux
+ *         utilisateurs
  */
 
 public class UtilisateurDAOJdbcImpl implements UtilisateurDAO {
 
+	/**
+	 * Déclaration des variables de classes
+	 * 
+	 * @Constante USER_INSERT -> String contenant la requête SQL permettant
+	 *            d'enregistrer un nouvel utilisateur dans la base de données
+	 * @Constante VERIF_USER_DATABASE -> String contenant la requête SQL permettant
+	 *            de vérifier un utilisateur est enregistré dans la base de données
+	 *            à l'aide de son mail et de son mot de passe
+	 * @Constante USER_INSERT -> String contenant la requête SQL permettant de
+	 *            vérifier un utilisateur est enregistré dans la base de données à
+	 *            l'aide de son pseudo et de son mot de passe
+	 */
+
 	private final String USER_INSERT = "INSERT INTO UTILISATEURS(pseudo,nom,prenom,email,telephone,rue,code_postal,ville,mot_de_passe) VALUES(?,?,?,?,?,?,?,?,?)";
 	private final String VERIF_USER_DATABASE = "SELECT pseudo,nom,prenom,email,telephone,rue,code_postal,ville,mot_de_passe,credit,administrateur FROM UTILISATEURS where email= ? AND mot_de_passe = ?";;
+	private final String VERIF_PSEUDO_DATABASE = "SELECT pseudo,nom,prenom,email,telephone,rue,code_postal,ville,mot_de_passe,credit,administrateur FROM UTILISATEURS where pseudo= ? AND mot_de_passe = ?";
 
 	/**
 	 * @author jarrigon2020
@@ -56,6 +74,58 @@ public class UtilisateurDAOJdbcImpl implements UtilisateurDAO {
 				String motDePasse = MyResultset.getString(9);
 
 				user = new Utilisateur(pseudo, nom, prenom, userEmail, telephone, rue, codePostal, ville, motDePasse);
+
+			}
+
+		} catch (SQLException e) {
+
+			throw new Exception("Erreur lors de l'ajout de l'utilisateur");
+
+		}
+
+		return user;
+
+	}
+
+	/**
+	 * @author jarrigon2020
+	 * @param pseudo   -> Chaine de caractère qui correspond au pseudo fourni par
+	 *                 l'utilisateur
+	 * @param password -> Chaine de caractère correspondant au mot de passe fourni
+	 *                 par l'utilisateur
+	 * @return user -> Objet de type Utilisateur
+	 * 
+	 *         Cette méthode permet de vérifier si le pseudo et le mot de passe
+	 *         fourni par l'utilisateur sont bien présents dans la base de données
+	 *         et correspondent entre eux.
+	 */
+	public Utilisateur userConnectionPseudo(String pseudo, String password) throws Exception {
+
+		ResultSet MyResultset = null;
+		Utilisateur user = null;
+
+		try (Connection databaseConnection = JdbcTools.getConnection();
+				PreparedStatement preparedStatement = databaseConnection.prepareStatement(VERIF_PSEUDO_DATABASE);) {
+
+			preparedStatement.setString(1, pseudo);
+			preparedStatement.setString(2, password);
+
+			MyResultset = preparedStatement.executeQuery();
+
+			if (MyResultset.next()) {
+
+				String userPseudo = MyResultset.getString(1);
+				String nom = MyResultset.getString(2);
+				String prenom = MyResultset.getString(3);
+				String userEmail = MyResultset.getString(4);
+				String telephone = MyResultset.getString(5);
+				String rue = MyResultset.getString(6);
+				String codePostal = MyResultset.getString(7);
+				String ville = MyResultset.getString(8);
+				String motDePasse = MyResultset.getString(9);
+
+				user = new Utilisateur(userPseudo, nom, prenom, userEmail, telephone, rue, codePostal, ville,
+						motDePasse);
 
 			}
 
